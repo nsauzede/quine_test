7c47e5fe2bef8b7c559020803037fe3e11b0b5c0
Nicolas Sauzede <nsauzede@laposte.net>
2020-11-13 22:56:34 +0100

    Add RS quine

---
+++ level1/quine.rs
@@ -0,0 +1,1 @@
+fn main(){let a="fn main(){let a=%c%s%c;unsafe{let(ptr,_):(*const i8,usize)=core::mem::transmute(a);extern%cC%c{pub fn printf(f:*const i8,...)->i32;}printf(ptr,34,ptr,34,34,34,10);}}%c";unsafe{let(ptr,_):(*const i8,usize)=core::mem::transmute(a);extern"C"{pub fn printf(f:*const i8,...)->i32;}printf(ptr,34,ptr,34,34,34,10);}}
