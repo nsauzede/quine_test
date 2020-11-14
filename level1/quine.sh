bcbfb0b44ceebd4bcb0e1c0a7a726bd73a2e5d2b
Nicolas Sauzede <nsauzede@laposte.net>
2020-11-14 21:18:41 +0100

    Simplify V and SH

--- level1/quine.sh
+++ level1/quine.sh
@@ -1,2 +1,1 @@
-a='a=%b%s%b%bb=%b%b%b%b;c=%b%bn%b;q="%b";printf "$a" $q "$a" $q $c $q $b $b $q $q $b $q $q $c%b'
-b='\\';c='\n';q="'";printf "$a" $q "$a" $q $c $q $b $b $q $q $b $q $q $c
+a='a=%b%s%b;b=%b%b%b%b;c=%b%bn%b;q="%b";printf "$a" $q "$a" $q $q $b $b $q $q $b $q $q $c%b';b='\\';c='\n';q="'";printf "$a" $q "$a" $q $q $b $b $q $q $b $q $q $c
--- level1/quine.v
+++ level1/quine.v
@@ -1,5 +1,2 @@
 import strconv
-a:='import strconv%ca:=%c%s%c%cc:=char(10)%cq:=char(39)%cstrconv.v_printf(a,c,q,a,q,c,c,c,c)%c'
-c:=char(10)
-q:=char(39)
-strconv.v_printf(a,c,q,a,q,c,c,c,c)
+a:='import strconv%ca:=%c%s%cc:=char(10)q:=char(39)strconv.v_printf(a,c,q,a,q,c)%c'c:=char(10)q:=char(39)strconv.v_printf(a,c,q,a,q,c)
